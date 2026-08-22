c65da521e4403bf203d3a0a433d91dea975fe589
hyeda2020 <hyeda2020@gmail.com>
2026-08-22 21:06:45 +0900

    페이징

--- src/main/java/hellojpa/JpqlMain.java
+++ src/main/java/hellojpa/JpqlMain.java
@@ -20,46 +20,15 @@ public class JpqlMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("member1");
-            member.setAge(30);
-            em.persist(member);
+//            Member member = new Member();
+//            member.setName("member1");
+//            member.setAge(30);
+//            em.persist(member);
 
             em.flush();
             em.clear();
 
-            /* 프로젝션 : SELECT 절에 조회할 대상을 지정하는 것  */
 
-            // 엔티티 프로젝션
-            List<Member> resultMemberList = em.createQuery("select m from Member m", Member.class)
-                    .getResultList();
-
-            // 엔티티 프로젝션
-            List<Team> resultTeamList = em.createQuery("select t from Member m join m.team t", Team.class)
-                    .getResultList();
-
-            // 임베디드 타입 프로젝션
-            List<Address> resultAddressList = em.createQuery("select m.address from Member m", Address.class)
-                    .getResultList();
-
-            // 스칼라 타입 프로젝션
-            List resultList = em.createQuery("select m.username, m.age from Member m")
-                    .getResultList();
-
-            // Object 타입을 통한 여러 값 조회
-//            for (Object o : resultList) {
-//                Object[] obj = (Object[]) o;
-//                System.out.println("username : " + obj[0]);
-//                System.out.println("age : " + obj[1]);
-//            }
-
-            // DTO를 통한 여러 값 조회
-            List<MemberDto> resultDtoList = em.createQuery("select new MemberDto(m.username, m.age) from Member m", MemberDto.class)
-                    .getResultList();
-
-            MemberDto memberDto = resultDtoList.get(0);
-            System.out.println("username : " + memberDto.getUsername());
-            System.out.println("age : " + memberDto.getAge());
 
             tx.commit();
         } catch (Exception e) {
@@ -70,6 +39,65 @@ public class JpqlMain {
         }
 
         emf.close();
+    }
+
+    private static void pagingQuery(EntityManager em) {
+
+        for (int i = 0; i < 100; i++) {
+            Member member = new Member();
+            member.setName("member" + i);
+            member.setAge(i);
+            em.persist(member);
+        }
+
+        em.flush();
+        em.clear();
+
+        // 페이징 API 예시
+        List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                .setFirstResult(1)
+                .setMaxResults(20)
+                .getResultList();
+
+        System.out.println("result.size = " + resultList.size());
+        for (Member member : resultList) {
+            System.out.println(member);
+        }
+    }
+
+    private static void projection(EntityManager em) {
+        /* 프로젝션 : SELECT 절에 조회할 대상을 지정하는 것  */
+
+        // 엔티티 프로젝션
+        List<Member> resultMemberList = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        // 엔티티 프로젝션
+        List<Team> resultTeamList = em.createQuery("select t from Member m join m.team t", Team.class)
+                .getResultList();
+
+        // 임베디드 타입 프로젝션
+        List<Address> resultAddressList = em.createQuery("select m.address from Member m", Address.class)
+                .getResultList();
+
+        // 스칼라 타입 프로젝션
+        List resultList = em.createQuery("select m.username, m.age from Member m")
+                .getResultList();
+
+        // Object 타입을 통한 여러 값 조회
+//            for (Object o : resultList) {
+//                Object[] obj = (Object[]) o;
+//                System.out.println("username : " + obj[0]);
+//                System.out.println("age : " + obj[1]);
+//            }
+
+        // DTO를 통한 여러 값 조회
+        List<MemberDto> resultDtoList = em.createQuery("select new MemberDto(m.username, m.age) from Member m", MemberDto.class)
+                .getResultList();
+
+        MemberDto memberDto = resultDtoList.get(0);
+        System.out.println("username : " + memberDto.getUsername());
+        System.out.println("age : " + memberDto.getAge());
     }
 
     private static void basicQuery(EntityManager em) {
